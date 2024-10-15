d1ef0696e1359f39cd7c7d1e2943d31bfde071a1
ShindinDN <viber.kat@outlook.com>
2024-10-15 23:01:28 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.ShindinDN.Sprint2.Task0.V14.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ShindinDN.Sprint2.Task0.V14.Lib
+{
+    public class DataService : ISprint2Task0V14
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+            res[0] = x - 321 == y;
+            res[1] = x - 321 != y;
+            res[2] = x - 1000 < y;
+            res[3] = x - 1000 > y;
+            res[4] = x - 800 <= y;
+            res[5] = x - 750 >= y;
+
+            return res;
+        }
+    }
+}
