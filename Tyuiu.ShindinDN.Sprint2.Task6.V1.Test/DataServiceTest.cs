bb822478cdb1ad286b7b3a240adbe439d7cfe750
ShindinDN <viber.kat@outlook.com>
2024-10-16 02:15:33 +0500

    Task 6

---
+++ Tyuiu.ShindinDN.Sprint2.Task6.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.ShindinDN.Sprint2.Task6.V1.Lib;
+
+namespace Tyuiu.ShindinDN.Sprint2.Task6.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int value = 5;
+            int res = ds.FindMonthDaysCount(value);
+            Assert.AreEqual(31, res);
+        }
+    }
+}
---
+++ Tyuiu.ShindinDN.Sprint2.Task6.V1/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.ShindinDN.Sprint2.Task6.V1.Lib;
+
+namespace Tyuiu.ShindinDN.Sprint2.Task6.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Шиндин Д. Н. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Получение результата из switch                                    *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Шиндин Денис Николаевич | ИСТНб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* По данному номеру месяца, определите количество дней в этом месяце.     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите номер месяца:");
+            int values = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.FindMonthDaysCount(values));
+        }
+    }
+}
